63a0ec1aa61769a1b502cab1f17b979f5596ad63
Elliott Sprehn <esprehn@gmail.com>
2017-03-31 13:09:44 -0700

    Expose a service from the worker

--- worker.js
+++ worker.js
@@ -1,4 +1,5 @@
 
+// Expose a service to the main thread.
 self.services.register('speak', class {
   static get exposed() { return ['concat']; }
 
@@ -10,3 +11,9 @@ self.services.register('speak', class {
     return this.prefix_ + message;
   }
 });
+
+// We can also connect to services the main thread exposes to us.
+(async function() {
+  let dom = await self.services.connect('dom');
+  dom.appendText('Inside the worker?');
+})();
