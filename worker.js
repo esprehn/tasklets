7ffde60a94f296b82c8405c583608a20dbdbfa81
Elliott Sprehn <esprehn@gmail.com>
2017-04-01 19:28:44 -0700

    Add some comments

--- worker.js
+++ worker.js
@@ -9,8 +9,10 @@ function weatherApiUrl(location) {
 
 // Expose a service to the main thread.
 self.services.register('speak', class {
+  // We have to list what methods are exposed.
   static get exposed() { return ['concat']; }
 
+  // The constructor can take arguments.
   constructor(prefix) {
     this.prefix_ = prefix;
   }
@@ -23,7 +25,10 @@ self.services.register('speak', class {
 self.services.register('weather', class {
   static get exposed() { return ['query']; }
 
+  // If a method returns a Promise (ex. is async) then the system waits for it
+  // to resolve and returns the answer.
   async query(location) {
+    // Make some network requests and do something "expensive".
     let response = await fetch(weatherApiUrl(location));
     let data = await response.json();
     let channel = data.query.results.channel;
@@ -38,6 +43,8 @@ self.services.register('weather', class {
 // We can also connect to services the main thread exposes to us.
 (async function() {
   let dom = await self.services.connect('dom');
+  // We can send multiple messages in parallel, since this is all inside one
+  // function they'll show up in the same order on the other thread.
   dom.appendText('Inside the worker?');
   dom.appendBox('A box!');
   dom.appendBox('A styled box!', new Map([
@@ -45,4 +52,8 @@ self.services.register('weather', class {
     ['border', '1px solid blue'],
     ['padding', '5px'],
   ]));
+  // For now we need to manually disconnect, eventually we might clean up the
+  // instances automatically and auto reconnect when a method is called if the
+  // end point has been cleaned up.
+  self.services.disconnect(dom);
 })();
